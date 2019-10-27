ae33713648a0f94413d873f7ec6c42c0d07a5d34
oussamaahmia <oussama-ahmia@hotmail.com>
2019-10-28 00:48:19 +0100

    Update Classification_example.py

--- examples/Classification_example.py
+++ examples/Classification_example.py
@@ -11,40 +11,40 @@ hierarchy = 2
 cpv_labels = json.load(open('fd-TED/cpv.json'))[language]
 
 
-        def cpv_hierarchy(cpv, hierarchy=2):
-                return [i[:hierarchy] + "0"*(8 - hierarchy) for i in cpv]
+def cpv_hierarchy(cpv, hierarchy=2):
+    return [i[:hierarchy] + "0"*(8 - hierarchy) for i in cpv]
 
 
-            def get_documents(path):
-                    data = []
-                        for i, doc in enumerate(gzip.open(path)):
-                                    l = []
-                                            doc_dict = json.loads(doc.decode())
-                                                    l.append(doc_dict['objet'])
-                                                            cpv = doc_dict['cpv']
-                                                                    if 'additional_information' in doc_dict:
-                                                                                    l.extend(doc_dict['additional_information'])
-                                                                                            if 'lots' in doc_dict:
-                                                                                                            if doc_dict['lots']:
-                                                                                                                                for lot in doc_dict['lots']:
-                                                                                                                                                        if 'cpv' in lot:
-                                                                                                                                                                                    cpv.extend(lot['cpv'])
-                                                                                                                                                                                                        l.append(lot['subject'])
-                                                                                                                                                                                                                            if 'desc' in lot:
-                                                                                                                                                                                                                                                        l.extend(lot['desc'])
-                                                                                                                                                                                                                                                                yield ("\n".join(l), cpv_hierarchy(cpv))
+def get_documents(path):
+    data = []
+    for i, doc in enumerate(gzip.open(path)):
+        l = []
+        doc_dict = json.loads(doc.decode())
+        l.append(doc_dict['objet'])
+        cpv = doc_dict['cpv']
+        if 'additional_information' in doc_dict:
+            l.extend(doc_dict['additional_information'])
+        if 'lots' in doc_dict:
+            if doc_dict['lots']:
+                for lot in doc_dict['lots']:
+                    if 'cpv' in lot:
+                        cpv.extend(lot['cpv'])
+                    l.append(lot['subject'])
+                    if 'desc' in lot:
+                        l.extend(lot['desc'])
+        yield ("\n".join(l), cpv_hierarchy(cpv))
 
 
-                                                                                                                                                                                                                                                                txt, y_ = zip(*get_documents('fd-TED/filtered/ted-FR.jsons.gz'))
+txt, y_ = zip(*get_documents('fd-TED/filtered/ted-FR.jsons.gz'))
 
-                                                                                                                                                                                                                                                                count = CountVectorizer().fit(txt)
-                                                                                                                                                                                                                                                                binarizer = MultiLabelBinarizer().fit(y_)
-                                                                                                                                                                                                                                                                X = count.transform(txt)
-                                                                                                                                                                                                                                                                y = binarizer.transform(y_)
+count = CountVectorizer().fit(txt)
+binarizer = MultiLabelBinarizer().fit(y_)
+X = count.transform(txt)
+y = binarizer.transform(y_)
 
-                                                                                                                                                                                                                                                                sgd = OneVsRestClassifier(SGDClassifier())
-                                                                                                                                                                                                                                                                sgd.fit(X, y)
+sgd = OneVsRestClassifier(SGDClassifier())
+sgd.fit(X, y)
 
-                                                                                                                                                                                                                                                                pred = sgd.predict(X)
-                                                                                                                                                                                                                                                                print(classification_report(y, pred, target_names=[
-                                                                                                                                                                                                                                                                          cpv_labels[i] for i in binarizer.classes_]))
+pred = sgd.predict(X)
+print(classification_report(y, pred, target_names=[
+      cpv_labels[i] for i in binarizer.classes_]))
